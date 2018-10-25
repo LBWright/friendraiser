a8955fe2fd1a24d54f01f6ab0dce4781f20ceda8
Logan Wright <logan.wright@valic.com>
2018-10-24 20:50:03 -0500

    initial security

---
+++ server/auth/restricted.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const restricted = (req, res, next) => {
+  const token = req.headers.authorization;
+  const secret = "this is the secret"; //pull this into it's own file
+
+  if(token){
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        return res
+          .status(401)
+          .json({ message: 'Token was not decoded', err });
+      } 
+      next();
+    });
+
+  } 
+  else{
+    res.send({message: "Error in retrieving token."})
+  }
+}
