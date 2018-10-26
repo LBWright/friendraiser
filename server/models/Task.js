5e2055aa4f37cb23a298e85b2a7cc33e1149ced4
Logan Wright <logan.wright@valic.com>
2018-10-26 10:27:18 -0500

    initial base schema

---
+++ server/models/Task.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
+
+const taskSchema = new Schema({
+    title: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    completed: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
